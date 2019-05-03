4e38273907b783e0ea2e1a20777fe4ab64d99308
Tristan Santiago <37146990+esotericwarrior@users.noreply.github.com>
2019-05-02 23:47:23 -0400

    Update chatClient.c
    Minor fixes and commenting.

--- chatClient.c
+++ chatClient.c
@@ -1,3 +1,9 @@
+/****************************************************************************
+* Name: Tristan Santiago
+* Date: May 2, 2019
+* Description: A simple client-server network chat application. Most of this
+* code has been modified and adopted from boilerplate code provided in CS344.
+****************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,14 +11,15 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
 
 void error(const char *msg) { perror(msg); exit(0); } // Error function used for reporting issues.
 void sendUserHandle(int socketFD, char userHandle[12], char *portNumber);
+int readMessage(int socketFD);
 int sendMessage(int socketFD, char userHandle[12]);
 
 int main(int argc, char *argv[]) {
-    int socketFD, portNumber, charsRead;
+    int socketFD, portNumber;   // Socket and port number.
     struct sockaddr_in serverAddress;
     struct hostent* serverHostInfo;
     char buffer[512];   // Buffer.
@@ -27,21 +34,21 @@ int main(int argc, char *argv[]) {
     serverAddress.sin_port = htons(portNumber); // Store the port number.
     serverHostInfo = gethostbyname(argv[1]); // Convert the machine name into a special form of address.
     if (serverHostInfo == NULL) { fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0); }
-    memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
+    memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address.
 
     // Set up the socket.
-    socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+    socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket.
     if (socketFD < 0) error("CLIENT: ERROR opening socket");
     
     // Connect to server.
-    if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
+    if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address.
         error("CLIENT: ERROR connecting");
 
     // Get handle from user.
     memset(userHandle, '\0', 12);
     printf("Please enter a username of no more than 10 characters: ");
     if((fgets(userHandle, 11, stdin) == NULL)) {    // If no handle is entered:
-        fprintf(stderr, "ERROR reading handle.\n"); // Throw an error.
+        fprintf(stderr, "ERROR reading user's handle.\n"); // Throw an error.
         exit(1);
     }
     userHandle[strcspn(userHandle, "\n")] = '\0'; // Remove the trailing \n that fgets adds.
@@ -62,25 +69,24 @@ int main(int argc, char *argv[]) {
     sendUserHandle(socketFD, userHandle, argv[2]);
     memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer array.
 
-    // Get return message from server.
-    memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer again for reuse.
-    charsRead = recv(socketFD, buffer, sizeof(buffer) - 1, 0); // Read data from the socket, leaving \0 at end.
-    if (charsRead < 0) error("CLIENT: ERROR reading from socket");  // Throw an error.
-    printf("%s\"\n", buffer);   // Print the message sent from the server.
-
-    if(strstr(buffer, "\\quit") != NULL) {  // If the user has entered the quit command:
-        printf("Connection terminated by the chat server.");    // Print message.
-        return -1;  // Exit the program.
-    }
-
-    sendMessage(socketFD, userHandle);
-
-
+    while(1) {
+        readMessage(socketFD);
+        sendMessage(socketFD, userHandle);
+    }   // End of while loop.
 
     close(socketFD); // Close the socket.
     return 0;
 }
 
+/****************************************************************************
+* Function: void sendUserHandle                                                                           
+* Parameters: int socketFD, char userHandle[12], char *portNumber
+* Description: This function is responsible for sending the user's handle
+* to the server and establishing the initial handshake before chatting begins.
+* It copies the user's handle (which has already been formatted according to 
+* the assignmnet's specification) into the buffer before concatenating the port
+* number and sending it to the server.
+*****************************************************************************/
 void sendUserHandle(int socketFD, char userHandle[12], char *portNumber) {
     int charsWritten;
     char buffer[512];
@@ -91,6 +97,39 @@ void sendUserHandle(int socketFD, char userHandle[12], char *portNumber) {
     if (charsWritten < strlen(buffer)) printf("CLIENT: WARNING: Not all data written to socket!\n");
 }
 
+/****************************************************************************
+* Function: int readMessage
+* Parameters: int socketFD
+* Description: This function receives messages from the server. If the server
+* sends the quit command, this function exits the program, otherwise it prints
+* the messages to the console.
+*****************************************************************************/
+int readMessage(int socketFD) {
+    int charsRead;
+    char buffer[512];
+    // Get return message from server.
+    memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer again for reuse.
+    charsRead = recv(socketFD, buffer, sizeof(buffer) - 1, 0); // Read data from the socket, leaving \0 at end.
+    if (charsRead < 0) error("CLIENT: ERROR reading from socket");  // Throw an error.
+    printf("%s\n", buffer);   // Print the message sent from the server.
+
+    if(strstr(buffer, "\\quit") != NULL) {  // If the user has entered the quit command:
+        printf("Connection terminated by the chat server.\n");    // Print message.
+        exit(-1);
+        //return -1;  // Exit the program.
+    }
+    return 0;
+}
+
+/****************************************************************************
+* Function: int sendMessage
+* Parameters: int socketFD, char userHandle[12]
+* Description: This function is responsible for sending the user's message to
+* the server. It displays the user's handle as a prompt, waiting for the user
+* to enter their message before sending it to the server. If the user enters
+* the quit command, this function sends the quit command to the server before
+* terminating the connection and exitting the program.
+****************************************************************************/
 int sendMessage(int socketFD, char userHandle[12]) {
     int charsWritten;
     char buffer[512];   // Buffer.
@@ -106,7 +145,7 @@ int sendMessage(int socketFD, char userHandle[12]) {
     printf(userHandle); // userHandle >
     if ((fgets(messageBuffer, 500-1, stdin) == NULL)) { // If no message is entered:
         fprintf(stderr, "ERROR reading message.\n");    // Throw an error.
-        return -1;  // Exit the program.
+        return -1;
     }
     // Remove the trailing \n that fgets adds.
     messageBuffer[strcspn(messageBuffer, "\n")] = 0;
@@ -119,12 +158,13 @@ int sendMessage(int socketFD, char userHandle[12]) {
 
     // Check to see if user has entered the quit command.
     if(strncmp(messageBuffer, "\\quit", 4) == 0) {
-        charsWritten = send(socketFD, buffer, strlen(buffer), 0); // Write to the server.
-        if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
+        charsWritten = send(socketFD, messageBuffer, strlen(messageBuffer), 0); // Write to the server.
+        if (charsWritten < 0) error("CLIENT: ERROR sending message to server.\n");
         if (charsWritten < strlen(buffer)) printf("CLIENT: WARNING: Not all data written to socket!\n");
         printf("Chat session ended.\n");
         close(socketFD);    // Close the socket.
         return -2;
+        //exit(1);
     }
     else {  // Otherwise, send the user's message.
         strcat(buffer, messageBuffer);  // Concatenate the message with the user's handle.
